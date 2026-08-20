7aa4ff7225f5c7d20de795d8e0cbad6db2056368
Andrii <andrii.kinash.aj@mail.com>
2026-08-20 17:53:31 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -2,15 +2,21 @@ import os
 
 
 def move_file(command: str) -> None:
-    list_of_comands = command.split()
-    if len(list_of_comands) != 3 or list_of_comands[0] != "mv":
+    list_of_commands = command.split()
+    if len(list_of_commands) != 3 or list_of_commands[0] != "mv":
         return
-    _, source, destination = list_of_comands
+    _, source, destination = list_of_commands
     if destination.endswith("/"):
         destination = os.path.join(destination, os.path.basename(source))
     directory = os.path.dirname(destination)
     if directory:
-        os.makedirs(directory, exist_ok=True)
-    with open(source, "r") as file_1, open(destination, "w") as file_2:
-        file_2.write(file_1.read())
+        current_directory = ""
+        for part in directory.split(os.sep):
+            current_directory = os.path.join(current_directory, part)
+            if not os.path.exists(current_directory):
+                os.mkdir(current_directory)
+    with open(source, "r") as source_file, open(
+        destination, "w"
+    ) as destination_file:
+        destination_file.write(source_file.read())
     os.remove(source)
